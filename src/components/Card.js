1d8d6f1b36b2b973e21c6b7d8a327400dc696f6d
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-04-15 22:59:37 -0700

    Fixed card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,13 +9,15 @@ const Card = ({ dailyReading }) => {
   const imgURL = `owf owf-${dailyReading.weather[0].id} owf-5x`;
 
   return (
-    <div>
-      <h3 className="card-title">{moment(today).format("dddd")}</h3>
-      <p className="text-muted">{moment(today).format("MMMM Do, h:mm a")}</p>
-      <i className={imgURL}></i>
-      <h2>{Math.round(dailyReading.main.temp)} °F</h2>
-      <div className="card-body">
-        <p className="card-text">{dailyReading.weather[0].description}</p>
+    <div className="col-sm-2">
+      <div className="card">
+        <h3 className="card-title">{moment(today).format("dddd")}</h3>
+        <p className="text-muted">{moment(today).format("MMMM Do, h:mm a")}</p>
+        <i className={imgURL}></i>
+        <h2>{Math.round(dailyReading.main.temp)} °F</h2>
+        <div className="card-body">
+          <p className="card-text">{dailyReading.weather[0].description}</p>
+        </div>
       </div>
     </div>
   );
