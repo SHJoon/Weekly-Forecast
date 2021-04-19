f9248c5cab71cf287367b97dda19d9e8cbf096a2
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-04-18 21:32:00 -0700

    Adding owfonts for logos

--- src/components/Forecast.js
+++ src/components/Forecast.js
@@ -4,11 +4,13 @@ import Card from "./Card";
 
 const Forecast = ({ forecast }) => {
   return (
-    <div>
+    <div className="container">
+      <h1 className="display-1 jumbotron">5-Day Forecast.</h1>
+      <h5 className="display-5 text-muted">New York, US</h5>
       {forecast?.cod === 200 ? (
         <div>
-          {forecast.map((dailyReading) => {
-            return <Card dailyReading={dailyReading} />;
+          {forecast.map((dailyReading, idx) => {
+            return <Card key={idx} dailyReading={dailyReading} />;
           })}
         </div>
       ) : null}
